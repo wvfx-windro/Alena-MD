5d288ff8a41b28334333259f8c5ba1852290e83a
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-05-29 07:21:49 +0530

    `MD COMMITS`

--- index.js
+++ index.js
@@ -81,8 +81,6 @@ async function startAlena() {
         if (mek.key && mek.key.remoteJid === 'status@broadcast') return
         if (!Alena.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
         if (mek.key.id.startsWith('BAE5') && mek.key.id.length === 16) return
-        m = smsg(Alena, mek, store)
-        require("./Gojosensei")(Alena, m, chatUpdate, store)
         } catch (err) {
             console.log(err)
         }
